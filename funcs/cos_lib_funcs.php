7a9b4d6be3ca3a3e41a81d411562ce496c96817b
Oliver Eglseder <oliver.eglseder@co-stack.com>
2021-06-24 10:54:09 +0200

    [CLEANUP] Remove unused code from factory and add missing throw annotations

--- funcs/cos_lib_funcs.php
+++ funcs/cos_lib_funcs.php
@@ -225,6 +225,8 @@ if (!function_exists('\CoStack\Lib\factory')) {
      * @return object
      *
      * @throws Exceptions\MissingConstructorArgumentException
+     * @throws Exceptions\MissingPropertyOrConstructorArgumentException
+     * @throws Exceptions\PropertyNotPublicException
      * @throws ReflectionException
      */
     function factory(string $class, array $arguments = []): object
@@ -233,13 +235,6 @@ if (!function_exists('\CoStack\Lib\factory')) {
         $constructorArgs = [];
 
         $constructor = $reflectionClass->getConstructor();
-        $properties = $reflectionClass->getProperties();
-        $publicProperties = [];
-        foreach ($properties as $index => $property) {
-            if ($property->isPublic()) {
-                $publicProperties[] = $property->getName();
-            }
-        }
 
         if (null !== $constructor) {
             foreach ($constructor->getParameters() as $reflectionParameter) {
